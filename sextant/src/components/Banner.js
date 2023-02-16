f71e59da671a147ffd6d8b63a018e0adfe9eb786
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2023-02-16 00:36:47 -0800

    adding banner

---
+++ sextant/src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../style/Banner.css";
+export default function Banner() {
+  return (
+    <div className="nav">
+      <h1 className="nav-text">Sextant</h1>
+    </div>
+  );
+}
